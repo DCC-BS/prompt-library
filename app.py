6ee0c9a477cd3ce3e2d92c15a292bb02a585ff60
Yanick Schraner <yanick.schraner@bs.ch>
2024-10-29 16:26:05 +0100

    Add more styling

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ from pages.create_page import show_create_page
 from pages.test_page import show_test_page
 
 
+__version__ = "1.0.0"
+__author__ = "data-alchemists des DigiLab BS"
+__author_email__ = "data-alchemists@bs.ch"
+VERSION_DATE = "2024-10-29"
+GIT_REPO = "https://github.com/opendatabs/prompt-library"
+
+
 def main():
     st.title("Prompt Library")
 
@@ -14,14 +21,36 @@ def main():
     if "page" not in st.session_state:
         st.session_state.page = "Browse Prompts"
 
+    st.logo("./assets/logo.jpg", size="large")
+    st.title("LLM Prompt Library")
+
     pg = st.navigation(
-        [
-            st.Page(show_browse_page, title="Browse Prompts", icon="📚"),
-            st.Page(show_create_page, title="Create New Prompt", icon="✏️"),
-            st.Page(show_test_page, title="Test Prompts", icon="🔬"),
-        ]
+        {
+            "LLM Prompt Library": [
+                st.Page(show_browse_page, title="Browse Prompts", icon="📚"),
+                st.Page(show_create_page, title="Create New Prompt", icon="✏️"),
+                st.Page(show_test_page, title="Test Prompts", icon="🔬"),
+            ]
+        }
     )
+
+    st.sidebar.image("./assets/logo.jpg")
+
+    show_info_box()
+
     pg.run()
+
+
+def show_info_box():
+    """
+    Displays an information box in the sidebar with author information, version number, and a link to the git repository.
+    """
+    impressum = f"""<div style="background-color:#34282C; padding: 10px;border-radius: 15px; border:solid 1px white;">
+    <small>Autoren: <a href="mailto:{__author_email__}">{__author__}</a><br>
+    Version: {__version__} ({VERSION_DATE})<br>
+    <a href="{GIT_REPO}">git-repo</a>
+    """
+    st.sidebar.markdown(impressum, unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
